82138dbfbd4d8b3560baa773db5f5369981e833b
Pushkar Saraf <pushkar.saraf.ossu@gmail.com>
2021-08-04 18:50:46 +0530

    Refactoring existing code added javadoc, formatting etc

--- src/main/java/com/incubyte/StringUtils.java
+++ src/main/java/com/incubyte/StringUtils.java
@@ -1,25 +1,45 @@
 package com.incubyte;
 
 public class StringUtils {
-    // private constructor for static class
-    // so default public one is not used
-    private StringUtils(){}
+    /**
+     * Private constructor for utility class
+     */
+    private StringUtils() {
+    }
 
-    public static boolean isEmpty(String input){
-        if(input == null) {
-            return  true;
+    /**
+     * Checks if input string is empty or null
+     *
+     * @param input input string
+     * @return outcome
+     */
+    public static boolean isEmpty(String input) {
+        if (input == null) {
+            return true;
         }
         return input.isBlank();
     }
 
+    /**
+     * Returns parsed numbers for input string
+     *
+     * @param input input string
+     * @return parsed numbers
+     */
     public static int[] getNumbers(String input) {
         int[] numbers = new int[3];
         numbers[0] = parseInt(input);
         return numbers;
     }
 
+    /**
+     * Method to parse an integer
+     *
+     * @param input number as string
+     * @return number as integer
+     */
     private static int parseInt(String input) {
-        try{
+        try {
             return Integer.parseInt(input);
         } catch (NumberFormatException ex) {
             return 0;
